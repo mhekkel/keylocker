2041113428c548e449d411a3d17d1f354cdde2c3
Maarten L. Hekkelman <maarten@hekkelman.com>
2015-11-23 15:55:05 +0100

    - Meer Settings

--- app/src/main/java/com/hekkelman/keylocker/KeyDetailActivity.java
+++ app/src/main/java/com/hekkelman/keylocker/KeyDetailActivity.java
@@ -1,27 +1,37 @@
 package com.hekkelman.keylocker;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.hekkelman.keylocker.com.hekkelman.keylocker.datamodel.Key;
 import com.hekkelman.keylocker.com.hekkelman.keylocker.datamodel.KeyDb;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class KeyDetailActivity extends AppCompatActivity {
 
     private String keyID;
+    private boolean textChanged = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,26 @@ public class KeyDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button btn = (Button) findViewById(R.id.gen_pw);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // get preferences
+                SharedPreferences prefs = getSharedPreferences("KeyLockerContext", Context.MODE_PRIVATE);
+                int length = prefs.getInt("password-length", 8);
+                boolean noAmbiguous = prefs.getBoolean("password-no-ambiguous", true);
+                boolean includeCapitals = prefs.getBoolean("password-include-capitals", true);
+                boolean includeDigits = prefs.getBoolean("password-include-digits", true);
+                boolean includeSymbols = prefs.getBoolean("password-include-symbols", true);
+
+                String pw = generatePassword(length, noAmbiguous, includeCapitals, includeDigits, includeSymbols);
+
+                EditText passwordFld = (EditText) findViewById(R.id.keyPasswordField);
+                passwordFld.setText(pw);
+            }
+        });
 
         this.keyID = getIntent().getStringExtra("keyId");
         if (this.keyID == null)
@@ -81,6 +111,50 @@ public class KeyDetailActivity extends AppCompatActivity {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+
+        int fieldIds[] = { R.id.keyNameField, R.id.keyPasswordField, R.id.keyUserField, R.id.keyURLField };
+
+        for (int fieldId : fieldIds) {
+            EditText field = (EditText) findViewById(fieldId);
+            field.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    KeyDetailActivity.this.textChanged = true;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (textChanged == false) {
+            super.onBackPressed();
+        }
+        else {
+            new AlertDialog.Builder(KeyDetailActivity.this)
+                    .setTitle(R.string.dlog_discard_changes_title)
+                    .setMessage(R.string.dlog_discard_changes_msg)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // do nothing
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
     }
 
@@ -138,6 +212,7 @@ public class KeyDetailActivity extends AppCompatActivity {
             if (name != null) {
                 KeyDb.getInstance().storeKey(keyID, name, user, password, url);
                 result = true;
+                this.textChanged = false;
             }
         } catch (Exception e) {
 
@@ -146,11 +221,96 @@ public class KeyDetailActivity extends AppCompatActivity {
         return result;
     }
 
-
 //    @Override
 //    protected void onPause() {
 //        KeyDb.setInstance(null);
 //
 //        super.onPause();
 //    }
+
+    private String generatePassword(int length, boolean noAmbiguous, boolean includeCapitals, boolean includeDigits, boolean includeSymbols)
+    {
+        final String kAmbiguous = "B8G6I1l0OQDS5Z2";
+
+        String[] vowels = getString(R.string.vowels).split(";");
+        String[] consonants = getString(R.string.consonants).split(";");
+        String result = "";
+
+        Random rng = new Random();
+
+        boolean vowel = rng.nextBoolean();
+        boolean wasVowel = false, hasDigits = false, hasSymbols = false, hasCapitals = false;
+
+        for (;;)
+        {
+            if (result.length() >= length)
+            {
+                if (result.length() > length ||
+                        includeDigits != hasDigits ||
+                        includeSymbols != hasSymbols ||
+                        includeCapitals != hasCapitals)
+                {
+                    result = "";
+                    hasDigits = hasSymbols = hasCapitals = false;
+                    continue;
+                }
+
+                break;
+            }
+
+            String s;
+            if (vowel)
+            {
+                do
+                    s = vowels[rng.nextInt(vowels.length)];
+                while (wasVowel && s.length() > 1);
+            }
+            else
+                s = consonants[rng.nextInt(consonants.length)];
+
+            if (s.length() + result.length() > length)
+                continue;
+
+            if (noAmbiguous && kAmbiguous.contains(s))
+                continue;
+
+            if (includeCapitals && (result.length() == s.length() || vowel == false) && rng.nextInt(10) < 2)
+            {
+                result += s.toUpperCase();
+                hasCapitals = true;
+            }
+            else
+                result += s;
+
+            if (vowel && (wasVowel || s.length() > 1 || rng.nextInt(10) > 3))
+                vowel = false;
+            else
+                vowel = true;
+
+            if (hasDigits == false && includeDigits && rng.nextInt(10) < 3)
+            {
+                String ch;
+                do ch = new Character((char)(rng.nextInt(10) + '0')).toString();
+                while (noAmbiguous && kAmbiguous.contains(ch));
+
+                result += ch;
+                hasDigits = true;
+            }
+            else if (hasSymbols == false && includeSymbols && rng.nextInt(10) < 2)
+            {
+                char[] kSymbols =
+                        {
+                                '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+',
+                                ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@',
+                                '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',
+                        };
+
+                result += kSymbols[rng.nextInt(kSymbols.length)];
+                hasSymbols = true;
+            }
+        }
+
+        return result;
+    }
+
 }
--- app/src/main/java/com/hekkelman/keylocker/SettingsActivity.java
+++ app/src/main/java/com/hekkelman/keylocker/SettingsActivity.java
@@ -158,7 +158,8 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
                 || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
-                || NotificationPreferenceFragment.class.getName().equals(fragmentName);
+                || NotificationPreferenceFragment.class.getName().equals(fragmentName)
+                || PasswordPreferenceFragment.class.getName().equals(fragmentName);
     }
 
     /**
@@ -251,4 +252,35 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             return super.onOptionsItemSelected(item);
         }
     }
+
+    /**
+     * This fragment shows general preferences only. It is used when the
+     * activity is showing a two-pane settings UI.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static class PasswordPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_password);
+            setHasOptionsMenu(true);
+
+            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
+            // to their values. When their values change, their summaries are
+            // updated to reflect the new value, per the Android Design
+            // guidelines.
+            bindPreferenceSummaryToValue(findPreference("password-length"));
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            if (id == android.R.id.home) {
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
+                return true;
+            }
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
